c043ef2ca813679ff0c1b46674842c6f166150c4
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-03-24 17:52:49 +0100

    stekje toevoegen

--- README.md
+++ README.md
@@ -28,20 +28,33 @@
 
 <!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met Javascript gedaan en hoe? Misschien heb je een framwork of library gebruikt? -->
 In deze leertaak hebben we weer gebruik gemaakt van een API. De API van Buurtcampus Oost
-is: [Link naar API Buurtcampus Oost ](https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes). Hierbij hebben we weer gebruik
-gemaakt van Node om server-side te kunnen programmeren. Met de templating taal EJS kan je de HTML-opmaak genereren met
-JavaScript. Dit zorgt er ook voor dat je in je partials maar een keer code hoeft te schrijven maar zich kan herhalen op
-je gehele website. Ook heb ik weer gebruik gemaakt van JSON. Daarnaast heb ik er voor gekozen om de mappen structuren
-zelf te installeren in plaats van mij oude project te kopiëren. Dit heb ik gedaan met Express en de $ npm init command.
+is: [Link naar API Buurtcampus Oost ](https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes). Hierbij hebben we weer
+gebruik gemaakt van Node om server-side te kunnen programmeren. Met de templating taal EJS kan je de HTML-opmaak
+genereren met JavaScript. Dit zorgt er ook voor dat je in je partials maar een keer code hoeft te schrijven maar zich
+kan herhalen op je gehele website. Ook heb ik weer gebruik gemaakt van JSON. Daarnaast heb ik er voor gekozen om de
+mappen structuren zelf te installeren in plaats van mij oude project te kopiëren. Dit heb ik gedaan met Express en de $
+npm init command.
 
-Wat is progressive enhancement: 
+Om gebruikers van de website data te kunnen toevoegen aan de website is er gebruikt gemaakt van de methode progressive
+enhancement. Met progressive enhancement bouw je je website in 3 lagen:
+
+1. Eerst bouw je de core functionality van je website in HTML en met behulp van formulieren en NodeJS, voor het
+   server-side afhandelen van het posten van data. (Content layer)
+2. Daarna voeg je CSS toe voor feedback voor de gebruiker en om de huisstijl toe te passen. (Presentation layer)
+3. Tot slot voeg je JavaScript (en CSS) toe om de User Experience te verbeteren. (Client-side scripting)
+
+Met progressive enhancement wordt er eerst gebouwd aan de basisfuncties, en vervolgens worden er stapsgewijs extra
+functies toegevoegd voor gebruikers met meer geavanceerde apparaten of browsers, terwijl de website nog steeds
+toegankelijk blijft voor alle gebruikers.
 
 ## Installatie
+
 Om mijn website te publiceren heb ik gebruik gemaakt van cyclic.sh als hostingpartij.
 
 ## Gebruik
 
 ## Bronnen
+
 https://www.cyclic.sh/
 https://nodejs.org/en/
 https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes
@@ -51,4 +64,5 @@ https://www.json.org/json-en.html
 https://statics.teams.cdn.office.net/evergreen-assets/safelinks/1/atp-safelinks.html
 
 ## Licentie
+
 This project is licensed under the terms of the [MIT license](./LICENSE).
--- index.js
+++ index.js
@@ -23,34 +23,59 @@ server.get('/', async function (req, res) {
     res.render('index', data)
 })
 
+// Route voor admin page
+server.get('/admin', (request, response) => {
+    response.render('admin')
+})
+
+// De .get()-methode wordt gebruikt om een routeringspad te definiëren.
+server.get('/:id', (request, response) => {
+    // Het externe API-eindpunt is opgeslagen in de baseUrl-variabele, terwijl de id-parameter wordt opgehaald uit de URL van de client met behulp van request.params.id.
+    const baseUrl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
+    let id = request.params.id
+    let stekjeUrl = baseUrl + '/stekjes?id=' + id
+    // de fetchJson()-functie opgeroepen, die het verzoek naar het externe API-eindpunt verzendt en wacht op een respons. Zodra de respons binnenkomt, wordt de data doorgegeven aan de response.render()-functie
+    fetchJson(stekjeUrl).then((data) => {
+        response.render('index', data)
+    })
+})
+
+// Wanneer een client een verzoek stuurt naar het '/new'-eindpunt, zal de response.render()-functie worden opgeroepen.
 server.get('/new', (request, response) => {
-    response.render('form')
+    response.render('admin')
 })
 
 server.post('/new', (request, response) => {
-    const baseurl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
-    const url = ('https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes')
-
+    // Het samenvoegen van de juiste API-eindpunten in de url-variabele.
+    const baseUrl = "https://api.buurtcampus-oost.fdnd.nl/api/v1"
+    const url = baseUrl + '/stekjes'
+    // request.body.aanmelddatum gedefinieerd als de huidige datum en tijd in een ISO-string-formaat met behulp van toISOString().
+    request.body.aanmelddatum = (new Date()).toISOString();
+    // postJson()-functie opgeroepen, die een HTTP POST-verzoek verzendt naar de opgegeven API-eindpunt en wacht op een respons. De postJson()-functie stuurt de gegevens uit het POST-verzoek op als JSON-geformatteerde gegevens in de request.body.
     postJson(url, request.body).then((data) => {
         let newStekje = {...request.body}
 
+        // Als het POST-verzoek succesvol was, wordt de client doorverwezen naar het hoofdmenu van de webapplicatie, zoals aangegeven door response.redirect('/').
         if (data.success) {
-            response.redirect('/?memberPosted=true')
-            // TODO: squad meegeven, message meegeven
-            // TODO: Toast meegeven aan de homepagina
+            response.redirect('/')
+
+        // Als het POST-verzoek niet succesvol was, wordt er een foutbericht gegenereerd met behulp van de inhoud van de respons.
         } else {
             const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
+            // De newStekje variabele wordt gedefinieerd om een kopie van de gegevens van het POST-verzoek te bevatten, en deze variabele wordt gebruikt in de response.render()-functie om ervoor te zorgen dat de gebruiker zijn oorspronkelijke invoer kan zien nadat de foutmelding is weergegeven.
             const newdata = {error: errormessage, values: newStekje}
-
-            response.render('form', newdata)
+            console.log(data)
+            console.log(JSON.stringify(data))
+            response.render('admin', newdata)
         }
     })
 })
 
 // Stel het poortnummer in
-server.set('port', 4000)
+server.set('port', 8000)
 
-// Start met luisteren
+// Start met luisteren naar binnenkomende HTTP-verzoeken op een bepaalde poort.
+// De server.get('port') functie haalt de waarde van de poort op die eerder is ingesteld met behulp van de server.set('port', value) functie.
 server.listen(server.get('port'), () => {
     console.log(`Application started on http://localhost:${server.get('port')}`)
 })
@@ -66,6 +91,9 @@ async function fetchJson(url) {
         .catch((error) => error)
 }
 
+// De fetchJson()-functie gebruikt de fetch()-functie om een HTTP GET-verzoek naar de opgegeven URL te versturen en wacht vervolgens op een respons.
+// Het gebruik van de async en await-sleutelwoorden zorgt ervoor dat de functie zal wachten totdat het HTTP-verzoek voltooid is voordat het resultaat geretourneerd wordt.
+
 /**
  * postJson() is a wrapper for the experimental node fetch api. It fetches the url
  * passed as a parameter using the POST method and the value from the body paramater
@@ -76,80 +104,17 @@ async function fetchJson(url) {
  */
 export async function postJson(url, body) {
     return await fetch(url, {
-        method: 'post',
-        body: JSON.stringify(body),
-        headers: {'Content-Type': 'application/json'},
+        method: 'post', body: JSON.stringify(body), headers: {'Content-Type': 'application/json'},
     })
         .then((response) => response.json())
         .catch((error) => error)
 }
 
+// Deze code definieert een asynchrone JavaScript-functie genaamd postJson. Deze functie verstuurt een HTTP POST-verzoek naar de opgegeven URL met behulp van de fetch()-functie van de browser-API.
 
-// Importeer express uit de node_modules map
-// import express from 'express'
+// De functie postJson accepteert twee parameters: url (de URL waarnaar het verzoek moet worden verzonden) en body (een JavaScript-object dat de gegevens bevat die moeten worden verzonden).
 
-// Maak een nieuwe express app aan
-// Van express een variabel app gemaakt
-// const app = express()
-
-// Stel ejs in als template engine en geef de 'views' map door
-// app.set('view engine', 'ejs')
-// app.set('views', './views')
-
-// Gebruik de map 'public' voor statische resources
-// app.use(express.static('public'))
-
-// Informatie krijgen
-// / routing voor de index
-// Maak een route voor de index
-// app.get('/', async function (req, res) {
-//     const url = ('https://api.buurtcampus-oost.fdnd.nl/api/v1/stekjes')
-//     const data = await fetch (url).then ((response)=> response.json())
-//     res.render('index', data)
-// })
-
-// app.get('/', (request, response) => {
-//     response.render('index')
-// })
-
-// app.get('/reservation', (request, response) => {
-//     // fetchen van data uit API, var veranderen naar data, de response met render functie voor de index de data erbij
-//     fetchJson(stekjesUrl).then((data) => {
-//         response.render('reservation', data)
-//     })
-// })
-
-// app.get('/workshops', (request, response) => {
-//     response.render('workshops')
-// })
-//
-// app.get('/contact', (request, response) => {
-//     response.render('contact')
-// })
-//
-// app.get('/hoe', (request, response) => {
-//     response.render('hoe')
-// })
-//
-// // Stel het poortnummer in en start express
-// app.set('port', process.env.PORT || 8000)
-// app.listen(app.get('port'), function () {
-//     console.log(`Application started on http://localhost:${app.get('port')}`)
-// })
-
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
-
-// async function fetchJson(url) {
-//     return await fetch(url)
-//         .then((response) => response.json())
-//         .catch((error) => error)
-// }
-
-
+// De functie wacht op de respons van de server en gebruikt vervolgens de .json() methode om de JSON-geformatteerde gegevens van de respons te extraheren.
 
 
 
